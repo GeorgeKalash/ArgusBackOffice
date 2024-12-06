5330eff999ffd8ad418d4b639ebb2b9defdf6afa
NoraBitarSM <Nora.Bitar@SoftMachine.co>
2024-12-06 12:46:52 +0200

    switch from argusTranslation to argusBackoffice

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ArgusTranslation app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Argus Backoffice app is running!');
   });
 });
